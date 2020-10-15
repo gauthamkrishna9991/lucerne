3637536b9607357b67f4739f1484ef268ac70ad6
Goutham Krishna K V <gauthamkrishna9991@live.com>
2020-10-16 00:34:39 +0530

    update TextureManager - 1

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -1,5 +1,6 @@
 #include "TextureManager.hpp"
 
+// this utility only creates textures for now, we can extend it later.
 SDL_Texture *TextureManager::LoadTexture(const char* filename, SDL_Renderer* ren) {
     SDL_Surface *tempsurface = IMG_Load(filename);
     SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, tempsurface);
